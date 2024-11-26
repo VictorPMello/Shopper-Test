3ab9d93539a7b5608c49d02ce60fc2840344ee33
Victor Mello <victormello7003@hotmail.com>
2024-11-26 15:49:48 -0300

    feat: Configurando API do google maps

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config()
 
 app.use(express.json())
 
@@ -8,21 +10,32 @@ app.get("/",
   (req: Request, res: Response): any => res.status(200).json({ message: "teste" })
 );
 
+
+
 app.post("/ride/estimate", async (req: Request, res: Response): Promise<any> => {
 
   const roadInfos = req.body;
 
   if (!roadInfos.hasOwnProperty("origin")) return res.status(400).json({ errorMessage: "A origem não foi informada!" })
   if (!roadInfos.hasOwnProperty("destination")) return res.status(400).json({ errorMessage: "O destino não foi informado!" })
-  if (!roadInfos.hasOwnProperty("id")) return res.status(400).json({ errorMessage: "User sem ID!" })
+  if (!roadInfos.hasOwnProperty("curstomer_id")) return res.status(400).json({ errorMessage: "User sem ID!" })
 
-  const { origin, destination, id } = roadInfos;
+  const { origin, destination, customer_id } = roadInfos;
 
   if (origin === destination) return res.status(400).json({ errorMessage: "Origem e destino não podem ser iguais!" })
 
 
-  // TODO: Validar se origem e destino são locais diferentes
   // TODO: Chamar API GOOGLE MAPS
+  const test = fetch('https://routes.googleapis.com/directions/v0:computeRoutes', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Goog-Api-Key': process.env.GOOGLE_API_KEY || '',
+      'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters,routes.polyline.encodedPolyline',
+    },
+    body: JSON.stringfy({})
+  })
+  console.log(test)
   // TODO: Enviar locais para API
   // TODO: Calcular distância
   // TODO: Validar ERROR caso aconteça
@@ -39,5 +52,7 @@ app.post("/ride/estimate", async (req: Request, res: Response): Promise<any> => 
 
   return res.status(500).json({ errorMessage: "error" })
 })
+
+
 
 export default app;
